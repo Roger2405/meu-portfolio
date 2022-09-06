119a4fd55aa72b0c8662f918f8406380193a280e
Roger <rogerdr.rdr@gmail.com>
2022-09-06 17:01:59 -0300

    finalizando o layout mobile

---
+++ js/projectGenerator.js
@@ -0,0 +1,64 @@
+
+
+//requisitando o arquivo JSON e armazenando-o na variável projectsJson quando receber a resposta;
+var projectsJson = await fetch("../files/projects.json")
+.then((response) => response.status == 200 && (projectsJson = response.json()))
+
+const projectsSection = document.querySelector('.projects'); //elemento pai de todos os projetos
+
+//verifica se a variável está preenchida;
+if(projectsJson) {
+    console.log('------------------');
+    //itera por todos os projetos do arquivo JSON e chama a função "montarProjeto" passando todos os dados necessários
+    projectsJson.forEach(projeto => {
+        createProject(projeto);
+    });
+}
+//se a variável não estiver preenchida, é executado o else, o qual adiciona uma mensagem de erro à projectsSection;
+else {
+    //projectsSection.appendChild(spanMensagemDeErro());
+}
+
+
+function createProject({name, url, iconSrc, screenshots} = typeof projectsJson[0]) {
+    
+    //criando os elementos HTML e os salvando em constantes para manipulá-los;
+    const divProject = document.createElement('div');//elemento pai
+    divProject.classList.add('project');
+
+    const link = document.createElement('a');
+    link.href = url;
+    link.classList.add('project__link');
+    link.target = '_blank';
+
+    
+    const divFooter = document.createElement('div');
+    divFooter.classList.add('project__footer');
+    
+    const iconElement =  document.createElement('img');
+    iconElement.src = iconSrc;
+    iconElement.classList.add('project__footer--icon');
+    const nameElement = document.createElement('h3');
+    nameElement.textContent = name;
+    nameElement.classList.add('project__footer--name');
+    if(screenshots) {
+        const imageBg = document.createElement('img');
+        imageBg.src = screenshots[0];
+        imageBg.alt = "Imagem do projeto " + name;
+        imageBg.classList.add('project__image');
+        divProject.appendChild(imageBg);
+        divFooter.style.backgroundColor = '#00000088';
+    }
+    else {
+        divFooter.style.backgroundColor = '#444444';
+    }
+    
+    divFooter.appendChild(iconElement);
+    divFooter.appendChild(nameElement);
+    
+    
+    divProject.appendChild(link);
+    divProject.appendChild(divFooter);
+
+    projectsSection.appendChild(divProject);
+}
---
+++ js/skillGenerator.js
@@ -0,0 +1,60 @@
+
+
+var skillsJson = await fetch("../files/skills.json")
+.then((resposta) => resposta.ok && (skillsJson = resposta.json()));
+
+const sectionSkills = document.querySelector('.skills');
+
+if(skillsJson) {
+    skillsJson.forEach(skill => {
+        createSkill(skill);
+    });
+}
+
+function createSkill({name, imgSrc, bgColor, expTime} = typeof skillsJson[0]) {
+    const divParent = document.createElement('div');
+    divParent.classList.add('skill');
+    divParent.style.backgroundColor = bgColor;
+
+    const imgSkill = document.createElement('img');
+    imgSkill.src = imgSrc;
+    imgSkill.classList.add('skill__image');
+
+    const nameSkill = document.createElement('h3');
+    nameSkill.textContent = name;
+    nameSkill.classList.add('skill__description--name');
+
+    const expTimeSkill = document.createElement('p');
+    expTimeSkill.classList.add('skill__description--exp');
+    expTimeSkill.textContent = `Tempo de experiência: ${convertedTime(expTime)}`;
+
+    const divDescriptionSkill = document.createElement('div');
+    divDescriptionSkill.classList.add('skill__description');
+    divDescriptionSkill.appendChild(nameSkill);
+    divDescriptionSkill.appendChild(expTimeSkill);
+
+    divParent.appendChild(imgSkill);
+    divParent.appendChild(divDescriptionSkill);
+    sectionSkills.appendChild(divParent);
+
+}
+function convertedTime(months) {
+    var intYears;
+    var intMonths = (months%12); ;
+    var text = "";
+
+    if(months >= 12) {
+        intYears = Math.floor(months/12);
+    }
+    
+    if(intYears) {
+        text = `${intYears.toString()} ${intYears>1 ? "anos" : "ano"}`;
+    }
+    if(intMonths){
+        if(text != "") {
+            text += " e ";
+        }
+        text += `${intMonths.toString()} ${intMonths > 1 ? "meses" : "mês"}`;
+    }
+    return text;
+}
