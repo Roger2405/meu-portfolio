415bdf9bbd83b5b2819a4d7b67a49a7b61277ce7
Roger <rogerdr.rdr@gmail.com>
2022-09-07 16:58:53 -0300

    ajustes ao gerar os elementos dos projetos

--- js/projectGenerator.js
+++ js/projectGenerator.js
@@ -5,72 +5,91 @@ var projectsJson = await fetch("../files/projects.json")
 .then((response) => response.status == 200 && (projectsJson = response.json()))
 
 const projectsSection = document.querySelector('.projects'); //elemento pai de todos os projetos
-const divProjectsWithoutImg = document.createElement('div');
-projectsSection.appendChild(divProjectsWithoutImg);
-divProjectsWithoutImg.classList.add('projects__noImg');
-divProjectsWithoutImg.classList.add('project');
+var divProjectsNoImg = createDivProjectsNoImg();
+var arrayDivProjectsNoImg = [];
+
+function createDivProjectsNoImg() {
+    const div = document.createElement('div');
+    div.classList.add('projects__noImg');
+    div.classList.add('project');
+    
+    return div;
+}
 
 //verifica se a variável está preenchida;
 if(projectsJson) {
-    console.log('------------------');
-    //itera por todos os projetos do arquivo JSON e chama a função "montarProjeto" passando todos os dados necessários
     projectsJson.forEach(projeto => {
         createProject(projeto);
     });
 }
-//se a variável não estiver preenchida, é executado o else, o qual adiciona uma mensagem de erro à projectsSection;
-else {
-    //projectsSection.appendChild(spanMensagemDeErro());
-}
-
 
 function createProject({name, url, iconSrc, screenshots} = typeof projectsJson[0]) {
     
     //criando os elementos HTML e os salvando em constantes para manipulá-los;
     const divProject = document.createElement('div');//elemento pai
     
-    const link = document.createElement('a');
-    link.href = url;
-    link.classList.add('project__link');
-    link.target = '_blank';
-
-    /*
-            
-            
-            */
+    const link = createLink(url);
+    const projectFooter = createFooter();
+    const icon = createIcon(iconSrc);
     
     
-    const divFooter = document.createElement('div');
-    divFooter.classList.add('project__footer');
     
-    const iconElement =  document.createElement('img');
-    iconElement.src = iconSrc;
-    iconElement.classList.add('project__footer--icon');
     const nameElement = document.createElement('h3');
     nameElement.textContent = name;
     nameElement.classList.add('project__footer--name');
     if(screenshots) {
-        const imageBg = document.createElement('img');
-        imageBg.src = screenshots[0];
-        imageBg.alt = "Imagem do projeto " + name;
-        imageBg.classList.add('project__image');
-        link.appendChild(imageBg);
-        link.appendChild(divFooter);
-        //divFooter.style.backgroundColor = '#00000088';
-        projectsSection.appendChild(divProject);
+        const imageScreenShots = createImg(screenshots);
+        imageScreenShots.alt = "Imagem do projeto " + name;
+        
         divProject.classList.add('project');
+        link.appendChild(imageScreenShots);
+        projectsSection.appendChild(divProject);
     }
     else {
-        divProjectsWithoutImg.appendChild(divProject);
-        projectsSection.appendChild(divProjectsWithoutImg);
-        //divFooter.style.backgroundColor = '#444444';
-        link.appendChild(divFooter)
+        divProjectsNoImg.appendChild(divProject);
+        if(divProjectsNoImg.childElementCount == 1) {
+            projectsSection.appendChild(divProjectsNoImg);
+        }
+        if(divProjectsNoImg.childElementCount == 2) {
+            console.log('limpando array');
+            divProjectsNoImg = createDivProjectsNoImg();
+            
+        }
+        
     }
-    
-    divFooter.appendChild(iconElement);
-    divFooter.appendChild(nameElement);
+    projectFooter.appendChild(icon);
+    projectFooter.appendChild(nameElement);
+    link.appendChild(projectFooter);
     
     
     divProject.appendChild(link);
     
+}
+function createLink(url) {
+    const link = document.createElement('a');
+    link.href = url;
+    link.classList.add('project__link');
+    link.target = '_blank';
+    
+    return link;
+}
+function createFooter() {
+    const divFooter = document.createElement('div');
+    divFooter.classList.add('project__footer');
+
+    return divFooter;
+}
+function createIcon(iconSrc) {
+    const iconElement =  document.createElement('img');
+    iconElement.src = iconSrc;
+    iconElement.classList.add('project__footer--icon');
+
+    return iconElement;
+}
+function createImg(images) {
+    const imageBg = document.createElement('img');
+    imageBg.src = images[0];
+    imageBg.classList.add('project__image');
+
+    return imageBg;
 }
